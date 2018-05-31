bfa14630c912f8a1e231959b843e532ab6abbfcc
sneezie510 <shuynh1300@yahoo.com>
2018-05-31 12:10:15 +0200

    testing lodash methods

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,7 +2,13 @@ console.log('Starting app.js')
 
 const fs = require('fs')
 const os = require('os')
+const _ = require('lodash')
 const notes = require('./notes.js')
+
+// console.log(_.isString(true))
+// console.log(_.isString('Sandi'))
+var filteredArray = _.uniq(['Sandi', 1, 'Sandi', 1, 2, 3, 4])
+console.log(filteredArray)
 
 var res = notes.add(1, 12)
   console.log(res)
